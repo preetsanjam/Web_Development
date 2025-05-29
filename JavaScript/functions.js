881efe71eef9468379c29c2a4f6d363c37092627
Sanjam Preet <sanjam.preet@gmail.com>
2025-05-29 15:48:58 -0400

    Added function expression syntax

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,3 +1,8 @@
+console.log(sum(213, 313)); // This code works
+
+//console.log(sum2(21,23));
+// The above code gives an error because it cannot be called before the function is decalred
+
 // Function declaration syntax
 function sum(x, y) { // x and y are local variables of sum function
     var result = x + y; // result is a local variable
