67ebdb034d181997e968c44232ad61fce7275b26
Sanjam Preet <sanjam.preet@gmail.com>
2025-05-29 14:23:56 -0400

    Added do while loops

--- JavaScript/looping.js
+++ JavaScript/looping.js
@@ -1,4 +1,6 @@
-// for loop
+// --------
+// FOR LOOP
+// --------
 
 // Incrementing loop
 for(var i= 1; i <= 10; i++) { // ++ operator increases the value of i by 1
@@ -28,7 +30,9 @@ for(var i = 3; i <= 30; i += 3) {
 
 console.log('Loop with a multiples of 3 stops')
 
-// while loop
+// ----------
+// WHILE LOOP
+// ----------
 
 // Incrementing loop
 var j = 1
@@ -47,3 +51,30 @@ while(k >= 1) {
 } 
 
 console.log('Decrementing while loop stops')
+
+// Loop with a step of 3
+var a = 1
+while(a <= 20) {
+    console.log(a);
+    a += 3;
+}
+
+console.log('While loop with a step of 3 stops')
+
+// -------------
+// DO WHILE LOOP
+// -------------
+
+// Printing numbers from 1 to 5
+var x = 1;
+do {
+    console.log('Count is: ' +x);
+    x++;
+} while(x <= 5);
+
+// Counting down from 5 to 1
+var num = 5;
+do {
+    console.log(num);
+    num--;
+} while(num > 0);
