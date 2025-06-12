fb1296ebc0721addcc621c1af4ad6b0c0d0b9234
Sanjam Preet <sanjam.preet@gmail.com>
2025-06-12 13:54:04 -0400

    Added another example of map()

--- JavaScript/advanced-js/array-iterator-methods.js
+++ JavaScript/advanced-js/array-iterator-methods.js
@@ -57,3 +57,35 @@ var returnHours = days.map(function(i) {
 
 console.log(returnHours);
 
+
+var scores = [
+    { subject: 'Math', score: 89 },
+    { subject: 'Programming', score: 100 },
+    { subject: 'Physics', score: 95 },
+    { subject: 'Chemistry', score: 75 },
+    { subject: 'History', score: 59 },
+];
+
+var gradedScores = scores.map(item => {
+    let grade;
+    if (item.score >= 91) {
+        grade = 'A';
+    } else if (item.score >= 81) {
+        grade = 'B';
+    } else if (item.score >= 71) {
+        grade = 'C';
+    } else {
+        grade = 'D';
+    }
+
+    return {
+        subject: item.subject,
+        score: item.score,
+        grade: grade
+    };
+});
+
+console.log(gradedScores);
+
+
+// Explore: some, every, reduce
