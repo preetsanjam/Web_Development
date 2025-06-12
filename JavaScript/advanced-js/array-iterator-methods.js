1feeabd22083ec1c0874e036a166bed707681e19
Sanjam Preet <sanjam.preet@gmail.com>
2025-06-12 02:18:31 -0400

    Added forEach iterator

---
+++ JavaScript/advanced-js/array-iterator-methods.js
@@ -0,0 +1,27 @@
+// Number of items an iterator function is called is equal to the number of items in an array
+
+// forEach()
+
+// Array of objects
+var days = [
+    {day: 'Sunday', hours: 2},
+    {day: 'Monday', hours: 8},
+    {day: 'Tuesday', hours: 7},
+    {day: 'Wednesday', hours: 10}
+]
+
+days.forEach(function(item) {
+    console.log(item)
+});
+
+var scores = [
+    { subject: 'Math', score: 89 },
+    { subject: 'Programming', score: 100 },
+    { subject: 'Physics', score: 95 },
+    { subject: 'Chemistry', score: 75 },
+    { subject: 'History', score: 59 },
+];
+
+scores.forEach(function(x) {
+    console.log(x)
+});
