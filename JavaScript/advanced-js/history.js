d01a91a1d06b874d0fa9c01dbf57ea6b0ab25ca0
Sanjam Preet <sanjam.preet@gmail.com>
2025-06-15 16:14:29 -0400

    Added methods of history object

--- JavaScript/advanced-js/history.js
+++ JavaScript/advanced-js/history.js
@@ -1,3 +1,10 @@
 // Run the code in the browser console
 
 console.log(history);
+
+// Three important methods of history object
+history.back();
+history.forward();
+history.go(-2); // Going 2 pages backwards
+
+history.pushState()
