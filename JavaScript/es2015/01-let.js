9b9b0b25b37834a83729024af515c68caab308d8
Sanjam Preet <sanjam.preet@gmail.com>
2025-06-30 18:28:56 -0400

    Added es2015 folder

---
+++ JavaScript/es2015/01-let.js
@@ -0,0 +1,16 @@
+let x = 1;
+console.log(x);
+
+
+// The following code throws an error
+// Returns "Identifier 'x' has already been declared"
+// let x = 2; 
+// console.log(x);
+
+// var x = 2; Throws an error
+
+var y = 1;
+console.log(y);
+
+var y = 2;
+console.log(y);
---
+++ JavaScript/es2015/notes.md
@@ -0,0 +1,5 @@
+## Notes
+
+- `let` vs `var`: 
+    
+    Variables declared by the `let` keyword must be declared before use and cannot be redeclared. Variables declared by the `var` keyword can be redeclared multiple times.  
