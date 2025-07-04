e47879d3c53f7bc438d91ba7e9398e8695ae5d07
Sanjam Preet <sanjam.preet@gmail.com>
2025-07-03 21:59:33 -0400

    Added syntax for const keyword

--- JavaScript/es2015/02-const.js
+++ JavaScript/es2015/02-const.js
@@ -2,4 +2,25 @@
 // SyntaxError: Missing initializer in const declaration
 
 const x = 1;
-// x = 2; Throws an error
+// x = 2; Throws an error because 'x' cannot be assigned another value
+
+const obj = {
+    y: 1
+};
+
+obj.y++; // Changing the value of 'y' property of 'obj'
+console.log(obj);
+
+// Remark:
+// Whenever an object is a const, it does not mean it is immutable
+// We can change the object
+// However, we cannot assign 'obj' to another object or value
+
+// The following code throws an error
+/* 
+obj = {
+    y: 2
+};
+*/
+
+// TypeError: Assignment to constant variable.
