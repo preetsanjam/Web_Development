50cc746fcd8b2c10e8154f2470e305d241c5c7cf
Sanjam Preet <sanjam.preet@gmail.com>
2025-07-09 18:40:07 -0400

    Added spread operator

--- JavaScript/es2015/08-spread.js
+++ JavaScript/es2015/08-spread.js
@@ -1,2 +1,4 @@
-const result = Math.max(56, 78, 3, 90, 34, 21);
+const numbers = [56, 78, 3, 90, 34, 21]
+const result = Math.max(...numbers); // Math.max() does not accept an array 
+                                    // We use the spread operator for calling a function 
 console.log(result);
