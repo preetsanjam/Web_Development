7d1e3889aefeffaaddf60db095072037b8ce7031
Sanjam Preet <sanjam.preet@gmail.com>
2025-07-11 18:59:28 -0400

    Added Map class

---
+++ JavaScript/es2015/12-Map.js
@@ -0,0 +1,11 @@
+const numbers = new Map([[1, 'one'], [2, 'two'], [3, 'three']])
+
+console.log(numbers.size);
+
+console.log(numbers.entries()); // entries() is the method of the Map class
+
+// Printing only the keys
+console.log(numbers.keys());
+
+// Printing only the values
+console.log(numbers.values());
