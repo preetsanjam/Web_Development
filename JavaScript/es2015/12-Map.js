02598b77a89ffe1ee9967f7775b45bbc31bbb44e
Sanjam Preet <sanjam.preet@gmail.com>
2025-07-11 23:35:10 -0400

    Added more methods of Map object

--- JavaScript/es2015/12-Map.js
+++ JavaScript/es2015/12-Map.js
@@ -9,3 +9,21 @@ console.log(numbers.keys());
 
 // Printing only the values
 console.log(numbers.values());
+
+// Adding a key-value pair
+numbers.set(40, 'forty');
+console.log(numbers);
+
+// See what is stored against key '3'
+// Acts as a lookup
+console.log(numbers.get(3));
+
+// Deleting a key-value pair
+numbers.delete(2);
+console.log(numbers);
+
+// numbers.clear() will remove all key-value pairs
+
+for(let [key, value] of numbers) {
+    console.log(`${key} = ${value}`);
+};
