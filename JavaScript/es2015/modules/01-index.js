dc6bc750dacedb275f553a1b321ae2d8ba232566
Sanjam Preet <sanjam.preet@gmail.com>
2025-07-19 19:12:03 -0400

    Added a default export

--- JavaScript/es2015/modules/01-index.js
+++ JavaScript/es2015/modules/01-index.js
@@ -3,3 +3,19 @@ export const diff = (x, y) => x - y;
 export var product = (x, y) => x * y;
 
 // sum, diff and product are global variables
+
+
+// We can have only one default export
+// For the default export, we don't need a name for it
+// Therefore, 'ComplexNumber' is optional
+// Like anonymous functions, we have anonymous classes 
+export default class ComplexNumber {
+    constructor(real, imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    toString() {
+        return `${this.real} + ${this.imaginary}i`;
+    }
+}; 
